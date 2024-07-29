cc5e9ba590daedac9382053fb8352721885b9f65
Ashishumarao <ashishumarao.dev@gamil.com>
2024-07-29 12:43:46 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+/** @format */
+import "../css/global.css";
+function Nav() {
+  return (
+    <div>
+      <ul className='common_flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Products</li>
+        <li>Contact us</li>
+      </ul>
+    </div>
+  );
+}
+export default Nav;
